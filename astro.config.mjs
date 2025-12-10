a3946b31b964e76f1c656c559d6fab91fe5e3fc7
Daniel Ruiz <Druiz8497@gmail.com>
2025-12-10 17:34:10 -0500

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    preview: {
+      allowedHosts: ['transporte.rv24.ovh'],
+    },
   },
 });
