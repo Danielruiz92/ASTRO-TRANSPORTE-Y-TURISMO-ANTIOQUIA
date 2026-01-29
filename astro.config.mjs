dced33b9b7559ff45a9a1dfccf61c46d936f9875
Daniel Ruiz <Druiz8497@gmail.com>
2026-01-29 16:32:28 -0500

    feat: add @astrojs/node dependency and update configuration for server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,22 +6,34 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.transporteyturismoantioquia.com',
+  output: 'server',
+
   build: {
     inlineStylesheets: 'always',
   },
+
   integrations: [react(), sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp',
     },
   },
+
   server: {
     allowedHosts: ['transporte.rv24.ovh', 'transporteyturismoantioquia.com', 'www.transporteyturismoantioquia.com', 'localhost'],
   },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
